75c04f3059cfd79285f5d04aa2d5399f453397f2
Matheus <andradematheus.dev@gmail.com>
2025-03-25 09:52:21 -0300

    internet sale adjustment

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -8,7 +8,7 @@ export const FaqSection = () => {
         <FaqItem title="Qual a base (Fundo de pigmento) das colorações?">Todas as coloração tem como base (fundo) AZUL/VIOLETA.</FaqItem>
         <FaqItem title="Como faço para ser um distribuidor?">É muito fácil, basta preencher o formulário nessa pagina que nossos consultores vaõ entrar em contato com você!</FaqItem>
         <FaqItem title="Qual o site oficial da empresa?"><a href="https://nannocosmetica.com.br" target="_blank" rel="noopener noreferrer">www.nannocosmetica.com.br</a></FaqItem>
-        <FaqItem title="Vocês vende os produtos na internet?">Não! Nossos produtos são exclusivos para salões de beleza e uso profissional. Por isso não vendemos nossos produtos em sites, marketplaces, perfumarias e afins.</FaqItem>
+        <FaqItem title="Vocês vende os produtos na internet?"><b>Não!</b> Nossos produtos são exclusivos para salões de beleza e uso profissional. Por isso não vendemos nossos produtos em sites, marketplaces, perfumarias e afins.</FaqItem>
         <FaqItem title="Qual o investimento minímo da primeira compra?">O investimento inicial para a distribuição é de <b>R$3.000,00</b></FaqItem>
         <FaqItem title="Qual a margem de lucratividade?">A margem de lucratividade da distribuição dos produtos varia de 100% a 120%.</FaqItem>
       </div>
