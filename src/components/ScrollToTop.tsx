90f73132cc2b7dd035e589a4cdde6c81e3162472
Matheus <andradematheus.dev@gmail.com>
2025-04-04 09:50:34 -0300

    scroll to top aria label fix

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -2,8 +2,8 @@ import { FaAngleUp } from "react-icons/fa";
 
 export const ScrollToTop = () => {
   return (
-    <div className="bg-neutral-500 text-white rounded-full fixed bottom-4 right-4" aria-label="Scroll to top">
-      <a href="#top">
+    <div className="bg-neutral-500 text-white rounded-full fixed bottom-4 right-4">
+      <a href="#top" aria-label="Subir para o topo da página">
         <FaAngleUp size={38} />
       </a>
     </div>
